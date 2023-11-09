b1008e7263d7d7af41dbf2be5c0b6a6fc08cf28f
chenxin <xin-chen22@mails.tsinghua.edu.cn>
2023-11-09 23:07:20 +0800

    good

--- src/tests/test_coroutine_ir.cpp
+++ src/tests/test_coroutine_ir.cpp
@@ -108,7 +108,7 @@ int main(int argc, char *argv[]) {
         };
         frame_buffer->write(id, frame);
     };
-    auto resume_shader = device.compile(resume_kernel, {.name = R"(C:\OldNew\Graphics-Lab\LuisaCompute\LuisaCompute-coroutine\output\esume_debug)"});
+    auto resume_shader = device.compile(resume_kernel, {.name = R"(C:\OldNew\Graphics-Lab\LuisaCompute\LuisaCompute-coroutine\output\resume_debug)"});
     stream << shader(x_buffer).dispatch(n)
            << x_buffer.copy_to(x_vec.data())
            << synchronize();

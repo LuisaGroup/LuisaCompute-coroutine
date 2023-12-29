9e2ea3b25fcddf3e0efbe927d0e809259c940206
Mike Smith <mango@live.cn>
2023-12-29 20:54:12 +0800

    fix warnings

--- src/rust/luisa_compute_ir/src/transform/canonicalize_control_flow.rs
+++ src/rust/luisa_compute_ir/src/transform/canonicalize_control_flow.rs
@@ -6,7 +6,6 @@
  * 3. Lower early-return nodes.
  */
 
-use crate::ir::debug::dump_ir_human_readable;
 use crate::ir::{
     collect_nodes, BasicBlock, Const, Func, Instruction, IrBuilder, Module, ModulePools, NodeRef,
     Type, INVALID_REF,
@@ -172,7 +171,7 @@ impl LowerGenericLoops {
                     }
                 }
                 Instruction::Continue => {
-                    if let Some(generic_loop_break) = self.generic_loop_break {
+                    if let Some(_) = self.generic_loop_break {
                         // continue => { break; }
                         let mut builder = IrBuilder::new(self.pools.clone());
                         builder.set_insert_point(node.get().prev);
@@ -756,7 +755,7 @@ struct LowerEarlyReturn {
 impl LowerEarlyReturn {
     fn glob_early_return_in_block(bb: &Pooled<BasicBlock>, is_top_level: bool) -> bool {
         bb.iter().any(|node| match node.get().instruction.as_ref() {
-            Instruction::Return(ret) => {
+            Instruction::Return(_) => {
                 // remove all nodes after the return node as they are unreachable
                 while !node.get().next.is_sentinel() {
                     node.get().next.remove();
@@ -1150,7 +1149,7 @@ impl CanonicalizeControlFlow {
                         load.remove();
                         node.replace_with(load.get());
                     }
-                    Instruction::Call(func, args) => {
+                    Instruction::Call(func, _) => {
                         if let Func::Callable(callable) = func {
                             Self::remove_phis_recursive(&callable.0.module, visited);
                         }
